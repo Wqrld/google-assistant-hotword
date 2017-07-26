cdd9584b92d95dd3603450ecc03692df7370a50e
Luc h <wereld03@gmail.com>
2017-07-26 14:51:07 +0200

    Commenting.

--- jarvis.py
+++ jarvis.py
@@ -29,6 +29,7 @@ from google.assistant.library.file_helpers import existing_file
 import sys
 import signal
 
+# setup detector
 model = 'jarvis.pmdl'
 import snowboydecoder
 detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
@@ -36,20 +37,9 @@ detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
 
 
 
-
-
-
-
-
-
-
-
-
-
+# interrupt handler (unneeded?)
 
 interrupted = False
-
-
 
 def signal_handler(signal, frame):
     global interrupted
@@ -61,12 +51,14 @@ def interrupt_callback():
     return interrupted
 
 
-
 signal.signal(signal.SIGINT, signal_handler)
 
 
 
+
+# mostly debug event processing
 def process_event(event):
+
     """Pretty prints events.
 
     Prints all events that occur with two spaces between each new
@@ -75,6 +67,7 @@ def process_event(event):
     Args:
         event(event.Event): The current event to process.
     """
+
     if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
         print()
 
@@ -85,6 +78,7 @@ def process_event(event):
         print()
 
 
+# parse credentials
 def main():
     global credentials
     parser = argparse.ArgumentParser(
@@ -100,6 +94,10 @@ def main():
     with open(args.credentials, 'r') as f:
         credentials = google.oauth2.credentials.Credentials(token=None,**json.load(f))
         print(credentials)
+
+
+# default assistant left in for debugging
+
  #   with Assistant(credentials) as assistant:
   #      for event in assistant.start():
    #         process_event(event)
@@ -110,28 +108,32 @@ if __name__ == '__main__':
 
 
 
+# when detector detects hotword.
 
 def detect_callback():
     with Assistant(credentials) as assistant:
-      print('ja baas?')
+      print('yes boss?')
       detector.terminate()
-      print("ik laat google maar zn werk doen")
+      print("google will help you")
       snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
       assistant.start_conversation()
       snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
       print("bye")
       detector.start(detected_callback=detect_callback, interrupt_check=interrupt_callback, sleep_time=0.03)
-      print("ik detect")
+      print("Listening for next wakewors")
 
 
 
 
+# start assistant non-listening so a conversation can be started
+# this part may give problems.
 
 with Assistant(credentials) as assistant:
   assistant.start()
   assistant.set_mic_mute(True)
 
 
+# start first wakewors detection
 detector.start(detected_callback=detect_callback, interrupt_check=interrupt_callback, sleep_time=0.03)
 
 
@@ -139,5 +141,5 @@ detector.start(detected_callback=detect_callback, interrupt_check=interrupt_call
 
 
 
-
+# terminates on interrupt, may be unneeded.
 detector.terminate()
